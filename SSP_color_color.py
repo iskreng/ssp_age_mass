dc42068400b2e18a971df2eca93f709f3ed20723
Iskren Georgiev <iskren.y.g@gmail.com>
2018-02-12 18:15:12 +0100

    update

--- SSP_color_color.py
+++ SSP_color_color.py
@@ -361,6 +361,8 @@ for row in data_phot.itertuples():
         #sbn.kdeplot(np.exp(ssp_model_0p2Z['ml_mod0p2Z']),bw=((.1*np.exp(ssp_model_0p2Z['ml_mod0p2Z'])).max()/2.),color='darkorange',label='KDE', vertical=True)
         ax22.plot(1e-9*10**(ssp_model_0p04Z['log_age_yr']),np.exp(ssp_model_0p04Z['ml_mod0p04Z']),linestyle='-',color='blue',label='0.04Z$_\odot$')
         ax22.set_xlim(1.,15); ax22.set_xticks(np.arange(2,16,2))#; ax22.yaxis.set_visible(False)
+        ax22.vlines(1e-9*10**(ssp_model_0p04Z['log_age_yr'][ssp_model_0p04Z['ml_mod0p04Z']==ssp_model_0p04Z['ml_mod0p04Z'].max()]),\
+                   np.exp(ssp_model_0p04Z['ml_mod0p04Z'].min()),np.exp(data['max_ml_0p04Z'].max()),linestyle='-', colors='gray', zorder=2, label='')
         ax22.legend(fontsize=10,ncol=2,columnspacing=.5,markerscale=0.28,framealpha=0)
         ax2 = fig.add_axes([.6,.39,.1750,.27])
         ax2.set_ylabel("Likelihood value")
@@ -372,7 +374,9 @@ for row in data_phot.itertuples():
         
         ax33 = fig.add_axes([.8125,.71,.1750,.27])
         #sbn.kdeplot(np.e**(ssp_model_Z['ml_modZ']),bw=(.1*np.e**(ssp_model_Z['ml_modZ']).max()/2.),color='red',label='KDE', vertical=True)
-        ax33.plot(1e-9*10**(ssp_model_2p5Z['log_age_yr']),np.exp(ssp_model_Z['ml_modZ']),linestyle='-',color='red',label='Z$_\odot$')
+        ax33.plot(1e-9*10**(ssp_model_Z['log_age_yr']),np.exp(ssp_model_Z['ml_modZ']),linestyle='-',color='red',label='Z$_\odot$')
+        ax33.vlines(1e-9*10**(ssp_model_Z['log_age_yr'][ssp_model_Z['ml_modZ']==ssp_model_Z['ml_modZ'].max()]),\
+                   np.exp(ssp_model_Z['ml_modZ'].min()),np.exp(data['max_ml_Z'].max()),linestyle='-', colors='gray', zorder=2, label='')
         ax33.set_xlim(1.,15); ax33.set_xticks(np.arange(2,16,2))#;ax33.yaxis.set_visible(False)
         ax33.legend(fontsize=10,ncol=2,columnspacing=.5,markerscale=0.28,framealpha=0)
         ax3 = fig.add_axes([.6,.71,.1750,.27])
